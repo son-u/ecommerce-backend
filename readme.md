ede65720aae8c05f4744c33b0df33c73811ee2da
Sonu <sonumrx37@gmail.com>
2024-03-02 21:21:12 +0530

    Created API for User Routes

---
+++ readme.md
@@ -0,0 +1,10 @@
+# This is BACKEND of  e-commerce project using MERN STACK
+### Backend started on 2nd March, 2024
+
+
+
+
+# Instruction to run the server
+1. npm init (to initialize)
+2. npm run dev (to start the e-commerce server)
+3. npm run watch (to watch all the changes being made)
---
+++ src/controllers/user.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import { User } from "../models/user.js";
+import { NewUserRequestBody } from "../types/types.js";
+
+export const newUser = async (
+  req: Request<{}, {}, NewUserRequestBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, photo, gender, _id, dob } = req.body;
+
+    const user = await User.create({
+      name,
+      email,
+      photo,
+      gender,
+      _id,
+      dob: new Date(dob),
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: `Welcome, ${user.name}`,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error,
+    });
+  }
+};
