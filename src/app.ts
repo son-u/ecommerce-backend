5afb08a7698f53e1a0dad92f692f67876d7ec069
Sonu <sonumrx37@gmail.com>
2024-03-09 22:36:02 +0530

    Integrated Stripe

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,12 @@ import { errorMiddleware } from "./middlewares/error.js";
 import NodeCache from "node-cache";
 import { config } from "dotenv";
 import morgan from "morgan";
+import Stripe from "stripe";
+
+
+
+
+
 
 // Importing Routes
 import userRoute from "./routes/user.js";
@@ -18,9 +24,12 @@ config({
 
 const port = process.env.PORT || 4000;
 const mongoURI = process.env.MONGO_URI || "";
+const stripeKey = process.env.STRIPE_KEY || "";
 
 connectDB(mongoURI);
 
+
+export const stripe = new Stripe(stripeKey)
 export const myCache = new NodeCache();
 
 const app = express();
